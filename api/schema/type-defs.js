4df70c4a3b3188e8932c33a2f23bd31f43932e5c
ankit-pn <miankitsingh@gmail.com>
2023-04-19 20:11:15 +0530

    Restructured the whole file directly to support vercel Changes to be committed: 	renamed:    config.json -> api/config.json 	renamed:    database/dbConnect.js -> api/database/dbConnect.js 	renamed:    database/mongoose-schema/taskModel.js -> api/database/mongoose-schema/taskModel.js 	renamed:    database/mongoose-schema/vmModel.js -> api/database/mongoose-schema/vmModel.js 	modified:   api/index.js 	renamed:    schema/resolvers.js -> api/schema/resolvers.js 	new file:   api/schema/type-defs.js 	renamed:    schema/type_defs.gql -> api/schema/type_defs.gql 	deleted:    schema/type-defs.js

---
+++ api/schema/type-defs.js
@@ -0,0 +1,55 @@
+import { gql } from 'apollo-server-express'
+import fs from 'fs';
+const typeDefs = gql`# It is gql file for server_manager
+
+
+type Task{
+    task_id: String!
+    taskName: String!
+    taskLength: Int!
+    communicationCost: Int!
+    taskDeadline: String!
+    server_id: String!
+    vm_id: String
+}
+type VM{
+    vm_id: String!
+    vmName: String!
+    vmMips: Int!
+    task: [Task!]
+    server_id: String!
+}
+type Server{
+    server_id: String!
+    server_name: String!
+    unit_power_cost: Int!
+    total_mips: Int!
+}
+
+
+input CreateVMInput{
+    vmName: String!
+    vmMips: Int!
+}
+
+input CreateTaskInput{
+    taskName: String!
+    taskLength: Int!
+    communicationCost: Int!
+    taskDeadline: String!
+    vm_id: String!
+}
+
+type Mutation{
+    create_vm(input: CreateVMInput!): [VM!]!
+    create_task(input: CreateTaskInput!): [Task!]!
+    # delete_vm(input: DeleteVMInput!): [VM!]!
+    # delete_task(input: DeleteTaskInput!): [Taks!]!
+}
+
+type Query {
+      server_details: Server!
+      vms: [VM!]!
+      tasks: [Task!]!
+}`;
+export { typeDefs };
--- schema/type-defs.js
+++
@@ -1,4 +0,0 @@
-import { gql } from 'apollo-server';
-import fs from 'fs';
-const typeDefs = gql(fs.readFileSync('schema/type_defs.gql', 'utf8'));
-export { typeDefs };
